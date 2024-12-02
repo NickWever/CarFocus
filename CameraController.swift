6f411390c9c2d3d3bb325aa906029372a7349a52
NickWever <snickwever@gmail.com>
2024-12-02 19:43:32 +0100

    Update CameraController.swift

--- CameraController.swift
+++ CameraController.swift
@@ -1,1 +1,62 @@
+//
+//  CameraController.swift
+//  CarFocus
+//
+//  Created by Nick Wever on 02/12/2024.
+//
+import SwiftUI
+import AVFoundation
+
+struct CameraController: View {
+    @ObservedObject var cameraManager = CameraManager()
+    @State private var cameraError: CameraManager.Error?
+
+    var body: some View {
+        ZStack {
+            if let error = cameraError {
+                createErrorStateView(error)
+            } else {
+                createCameraView()
+            }
+        }
+        .onAppear(perform: checkCameraPermissions)
+        .onChange(of: UIDevice.current.orientation) { _ in
+            cameraManager.updatePreviewOrientation()
+        }
+    }
+
+    private func checkCameraPermissions() {
+        do {
+            try cameraManager.checkPermissions()
+        } catch {
+            cameraError = error as? CameraManager.Error
+        }
+    }
+
+    private func createErrorStateView(_ error: CameraManager.Error) -> some View {
+        Text("Camera Error: \(error.localizedDescription)")
+    }
+
+    private func createCameraView() -> some View {
+        CameraInputView(cameraManager: cameraManager)
+    }
+}
+
+struct CameraInputView: UIViewRepresentable {
+    let cameraManager: CameraManager
+
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        cameraManager.setup(in: view)
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
+
+struct CameraController_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraController()
+    }
+}
 
