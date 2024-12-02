532f96c39646edd8e855d12e87b4b048c5e4e6fb
NickWever <snickwever@gmail.com>
2024-12-02 19:20:16 +0100

    Update CameraManager.swift

--- CameraManager.swift
+++ CameraManager.swift
@@ -1,1 +1,421 @@
+import SwiftUI
+import AVFoundation
+import UIKit
+
+// CameraManager with device orientation handling
+class CameraManager: NSObject, ObservableObject {
+    // MARK: Devices
+    private var frontCamera: AVCaptureDevice?
+    private var backCamera: AVCaptureDevice?
+
+    // MARK: Input
+    private var captureSession: AVCaptureSession!
+    private var frontCameraInput: AVCaptureDeviceInput?
+    private var backCameraInput: AVCaptureDeviceInput?
+
+    // MARK: Output
+    private var photoOutput: AVCapturePhotoOutput?
+
+    // MARK: UI Elements
+    private(set) var cameraLayer: AVCaptureVideoPreviewLayer!
+    @Published private(set) var isGridVisible: Bool = true
+    private(set) var cameraGridView: GridView!
+
+    // MARK: Attributes
+    @Published private(set) var capturedImage: Data? = nil
+    @Published private(set) var cameraPosition: CameraPosition = .back
+    @Published private(set) var flashMode: CameraFlashMode = .off
+    @Published private(set) var torchMode: CameraTorchMode = .off
+
+    // MARK: Orientation Handling
+    override init() {
+        super.init()
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(deviceOrientationDidChange),
+            name: UIDevice.orientationDidChangeNotification,
+            object: nil
+        )
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func deviceOrientationDidChange() {
+        updatePreviewOrientation()
+    }
+
+    func updatePreviewOrientation() {
+        guard let connection = cameraLayer?.connection else { return }
+        guard connection.isVideoRotationAngleSupported else { return }
+
+        switch UIDevice.current.orientation {
+        case .portrait:
+            connection.videoRotationAngle = 0
+        case .landscapeLeft:
+            connection.videoRotationAngle = 90
+        case .landscapeRight:
+            connection.videoRotationAngle = -90
+        case .portraitUpsideDown:
+            connection.videoRotationAngle = 180
+        default:
+            connection.videoRotationAngle = 0
+        }
+
+        DispatchQueue.main.async {
+            self.cameraLayer?.frame = UIScreen.main.bounds
+        }
+    }
+
+    // Setup methods
+    func setup(in cameraView: UIView) throws {
+        initialiseCaptureSession()
+        initialiseCameraLayer(cameraView)
+        initialiseDevices()
+        initialiseInputs()
+        initialiseOutputs()
+
+        try setupDeviceInputs()
+        try setupDeviceOutput()
+
+        startCaptureSession()
+        announceSetupCompletion()
+        initialiseCameraGridView()
+    }
+
+    func initialiseCaptureSession() {
+        captureSession = .init()
+    }
+
+    func initialiseCameraLayer(_ cameraView: UIView) {
+        cameraLayer = .init(session: captureSession)
+        cameraLayer.videoGravity = .resizeAspectFill
+        cameraView.layer.addSublayer(cameraLayer)
+    }
+
+    func initialiseDevices() {
+        frontCamera = .default(.builtInWideAngleCamera, for: .video, position: .front)
+        backCamera = .default(for: .video)
+    }
+
+    func initialiseInputs() {
+        frontCameraInput = try? AVCaptureDeviceInput(device: frontCamera)
+        backCameraInput = try? AVCaptureDeviceInput(device: backCamera)
+    }
+
+    func initialiseOutputs() {
+        photoOutput = .init()
+    }
+
+    func setupDeviceInputs() throws {
+        try setupCameraInput(.back)
+    }
+
+    func setupDeviceOutput() throws {
+        try setupCameraOutput(.photo)
+    }
+
+    func startCaptureSession() {
+        DispatchQueue(label: "cameraSession").async {
+            self.captureSession.startRunning()
+        }
+    }
+
+    func announceSetupCompletion() {
+        DispatchQueue.main.async {
+            self.objectWillChange.send()
+        }
+    }
+
+    func setupCameraInput(_ cameraPosition: CameraPosition) throws {
+        switch cameraPosition {
+        case .front:
+            try setupInput(frontCameraInput)
+        case .back:
+            try setupInput(backCameraInput)
+        }
+    }
+
+    func setupCameraOutput(_ outputType: CameraOutputType) throws {
+        if let output = getOutput(outputType) {
+            try setupOutput(output)
+        }
+    }
+
+    func setupInput(_ input: AVCaptureDeviceInput?) throws {
+        guard let input, captureSession.canAddInput(input) else {
+            throw Error.cannotSetupInput
+        }
+        captureSession.addInput(input)
+    }
+
+    func setupOutput(_ output: AVCaptureOutput?) throws {
+        guard let output, captureSession.canAddOutput(output) else {
+            throw Error.cannotSetupOutput
+        }
+        captureSession.addOutput(output)
+    }
+    
+    func initialiseCameraGridView() {
+        cameraGridView = .init()
+        cameraGridView.addAsSubview(to: cameraView)
+        cameraGridView.alpha = isGridVisible ? 1 : 0
+    }
+
+    func changeGridVisibility(_ shouldShowGrid: Bool) {
+        animateGridVisibilityChange(shouldShowGrid)
+        updateGridVisibility(shouldShowGrid)
+    }
+
+    func animateGridVisibilityChange(_ shouldShowGrid: Bool) {
+        UIView.animate(withDuration: 0.32) {
+            self.cameraGridView.alpha = shouldShowGrid ? 1 : 0
+        }
+    }
+
+    func updateGridVisibility(_ shouldShowGrid: Bool) {
+        isGridVisible = shouldShowGrid
+    }
+
+    func capturePhoto() {
+        let settings = getPhotoOutputSettings()
+        configureOutput(photoOutput)
+        photoOutput?.capturePhoto(with: settings, delegate: self)
+    }
+
+    func getPhotoOutputSettings() -> AVCapturePhotoSettings {
+        let settings = AVCapturePhotoSettings()
+        settings.flashMode = flashMode.get()
+        return settings
+    }
+
+    func changeCamera(_ newPosition: CameraPosition) throws {
+        if newPosition != cameraPosition {
+            removeCameraInput(cameraPosition)
+            try setupCameraInput(newPosition)
+            updateCameraPosition(newPosition)
+        }
+    }
+
+    func removeCameraInput(_ position: CameraPosition) {
+        if let input = getInput(position) {
+            captureSession.removeInput(input)
+        }
+    }
+
+    func updateCameraPosition(_ position: CameraPosition) {
+        cameraPosition = position
+    }
+
+    func getInput(_ position: CameraPosition) -> AVCaptureInput? {
+        switch position {
+        case .front:
+            return frontCameraInput
+        case .back:
+            return backCameraInput
+        }
+    }
+
+    func changeFlashMode(_ mode: CameraFlashMode) throws {
+        if let device = getDevice(cameraPosition), device.hasFlash {
+            updateFlashMode(mode)
+        }
+    }
+
+    func updateFlashMode(_ value: CameraFlashMode) {
+        flashMode = value
+    }
+
+    func changeTorchMode(_ mode: CameraTorchMode) throws {
+        if let device = getDevice(cameraPosition), device.hasTorch {
+            try changeTorchMode(device, mode)
+            updateTorchMode(mode)
+        }
+    }
+
+    func changeTorchMode(_ device: AVCaptureDevice, _ mode: CameraTorchMode) throws {
+        try device.lockForConfiguration()
+        device.torchMode = mode.get()
+        device.unlockForConfiguration()
+    }
+
+    func updateTorchMode(_ value: CameraTorchMode) {
+        torchMode = value
+    }
+
+    enum Error: Swift.Error {
+        case cannotSetupInput
+        case cannotSetupOutput
+        case cameraPermissionsNotGranted
+    }
+}
+
+// UIDeviceOrientation extensions
+extension UIDeviceOrientation {
+    var rotationAngle: CGFloat {
+        switch self {
+        case .portrait:
+            return 0
+        case .landscapeLeft:
+            return 90
+        case .landscapeRight:
+            return -90
+        case .portraitUpsideDown:
+            return 180
+        default:
+            return 0
+        }
+    }
+
+    var videoOrientation: AVCaptureVideoOrientation? {
+        switch self {
+        case .portrait:
+            return .portrait
+        case .landscapeLeft:
+            return .landscapeRight
+        case .landscapeRight:
+            return .landscapeLeft
+        case .portraitUpsideDown:
+            return .portraitUpsideDown
+        default:
+            return nil
+        }
+    }
+}
+
+// GridView class and extensions
+class GridView: UIView {}
+
+extension GridView {
+    func addAsSubview(to view: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = UIColor.clear
+
+        view.addSubview(self)
+
+        leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+        rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
+        topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+    }
+}
+
+extension GridView {
+    override func draw(_ rect: CGRect) {
+        let firstColumnPath = UIBezierPath()
+        firstColumnPath.move(to: CGPoint(x: bounds.width / 3, y: 0))
+        firstColumnPath.addLine(to: CGPoint(x: bounds.width / 3, y: bounds.height))
+        let firstColumnLayer = createGridLayer()
+        firstColumnLayer.path = firstColumnPath.cgPath
+        layer.addSublayer(firstColumnLayer)
+
+        let secondColumnPath = UIBezierPath()
+        secondColumnPath.move(to: CGPoint(x: (2 * bounds.width) / 3, y: 0))
+        secondColumnPath.addLine(to: CGPoint(x: (2 * bounds.width) / 3, y: bounds.height))
+        let secondColumnLayer = createGridLayer()
+        secondColumnLayer.path = secondColumnPath.cgPath
+        layer.addSublayer(secondColumnLayer)
+
+        let firstRowPath = UIBezierPath()
+        firstRowPath.move(to: CGPoint(x: 0, y: bounds.height / 3))
+        firstRowPath.addLine(to: CGPoint(x: bounds.width, y: bounds.height / 3))
+        let firstRowLayer = createGridLayer()
+        firstRowLayer.path = firstRowPath.cgPath
+        layer.addSublayer(firstRowLayer)
+
+        let secondRowPath = UIBezierPath()
+        secondRowPath.move(to: CGPoint(x: 0, y: (2 * bounds.height) / 3))
+        secondRowPath.addLine(to: CGPoint(x: bounds.width, y: (2 * bounds.height) / 3))
+        let secondRowLayer = createGridLayer()
+        secondRowLayer.path = secondRowPath.cgPath
+        layer.addSublayer(secondRowLayer)
+    }
+}
+
+private extension GridView {
+    func createGridLayer() -> CAShapeLayer {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.strokeColor = UIColor(white: 1.0, alpha: 0.28).cgColor
+        shapeLayer.frame = bounds
+        shapeLayer.fillColor = nil
+        return shapeLayer
+    }
+}
+
+// CameraController with integrated orientation handling
+struct CameraController: View {
+    @ObservedObject var cameraManager: CameraManager = .init()
+    @State var cameraError: CameraManager.Error?
+
+    var body: some View {
+        ZStack {
+            switch cameraError {
+            case .some(let error):
+                createErrorStateView(error)
+            case nil:
+                createCameraView()
+        }}
+        .onAppear(perform: checkCameraPermissions)
+        .onChange(of: UIDevice.current.orientation) { _ in
+            cameraManager.updatePreviewOrientation()
+        }
+    }
+}
+
+private extension CameraController {
+    func checkCameraPermissions() {
+        do {
+            try cameraManager.checkPermissions()
+        } catch {
+            cameraError = error as? CameraManager.Error
+        }
+    }
+
+    func createErrorStateView(_ error: CameraManager.Error) -> some View {
+        Text("Camera Error: \(error.localizedDescription)")
+    }
+
+    func createCameraView() -> some View {
+        CameraInputView(cameraManager)
+    }
+}
+
+// CameraInputView with integrated orientation handling
+struct CameraInputView: UIViewRepresentable {
+    let cameraManager: CameraManager
+    private var inputView: UICameraInputView = .init()
+
+    init(_ cameraManager: CameraManager) {
+        self.cameraManager = cameraManager
+    }
+
+    func makeUIView(context: Context) -> some UIView {
+        inputView.cameraManager = cameraManager
+        return inputView.view
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {}
+}
+
+// UICameraInputView with integrated orientation handling
+fileprivate class UICameraInputView: UIViewController {
+    var cameraManager: CameraManager!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCameraManager()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        cameraManager.cameraLayer.frame = view.bounds
+    }
+
+    private func setupCameraManager() {
+        do {
+            try self.cameraManager.setup(in: view)
+        } catch {}
+    }
+}
 
